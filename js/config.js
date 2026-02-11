48562b1db77c6f2ba58427949e0b71d5f0b46634
SAYOOJ KS <118405061+sayooj100@users.noreply.github.com>
2026-02-11 16:07:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,11 +2,12 @@ const clientData = {
   herName: "Meera",
   boyfriendName: "Rahul",
   photos: [
-    "assets/photo1.jpg", 
-    "assets/photo2.jpg", 
-    "assets/photo3.jpg"
+    "assets/img1.jpg", 
+    "assets/img2.jpg", 
+    "assets/img3.jpg"
   ],
   letterMessage: "Since the day I met you, my life has been a beautiful melody. You are the sunshine that brightens my darkest days and the stars that guide me home. Every moment with you is a treasure I hold dear. Getting to celebrate you today is the greatest gift of all. I love you more than words can say.",
   birthdayDate: "March 15",
   musicFile: "assets/music.mp3"
 };
+
